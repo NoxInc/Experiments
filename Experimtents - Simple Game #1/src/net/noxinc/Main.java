af32c03521ea51e26e5120cc25f5209fde57e677
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-18 15:17:59 +0100

    Added playerInventory

--- Experimtents - Simple Game #1/src/net/noxinc/Main.java
+++ Experimtents - Simple Game #1/src/net/noxinc/Main.java
@@ -11,9 +11,10 @@ public class Main
 	public static void main(String[] args)
 	{
 		Map map  = new Map(10, 10);
-		Player player = new Player(map, 2, 3);
+		Player player = new Player(map, 2, 3, 5);
 		
 		map.redraw();
+		player.getInventory().openInventory();
 		
 	}
 
