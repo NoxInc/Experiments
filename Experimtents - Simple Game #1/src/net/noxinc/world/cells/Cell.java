966e057310d84970d15d78d5f1f6a368330684db
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-18 22:51:36 +0100

    Collect cell frame

--- Experimtents - Simple Game #1/src/net/noxinc/world/cells/Cell.java
+++ Experimtents - Simple Game #1/src/net/noxinc/world/cells/Cell.java
@@ -3,6 +3,7 @@ package net.noxinc.world.cells;
 public class Cell 
 {
 	private char symbol;
+	private boolean collectable;
 	
 	public Cell()
 	{
@@ -12,6 +13,17 @@ public class Cell
 	public char getSymbol()
 	{
 		return symbol;
+	}
+	
+	public boolean isCollectable()
+	{
+		return collectable;
+	}
+	
+	public Cell setCollectable(boolean value)
+	{
+		collectable = value;
+		return this;
 	}
 	
 	public Cell setSymbol(char symbol)
