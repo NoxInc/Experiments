3288dd24b4d6b21aef1f79662984c14965bc9c7f
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-18 20:50:23 +0100

    init DebugCell

---
+++ Experimtents - Simple Game #1/src/net/noxinc/world/cells/DebugCell.java
@@ -0,0 +1,16 @@
+package net.noxinc.world.cells;
+
+public class DebugCell extends Cell
+{
+	private char symbol = 'D';
+	
+	public DebugCell()
+	{
+		
+	}
+
+	public char getSymbol()
+	{
+		return symbol;
+	}
+}
