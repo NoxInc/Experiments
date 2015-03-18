93ad6a117b64005c9254b14669426c047f420e63
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-18 20:50:12 +0100

    Init movement and cleanup

--- Experimtents - Simple Game #1/src/net/noxinc/Main.java
+++ Experimtents - Simple Game #1/src/net/noxinc/Main.java
@@ -2,20 +2,27 @@ package net.noxinc;
 
 import java.util.Scanner;
 
+import net.noxinc.dialogs.Dialogs;
 import net.noxinc.units.Player;
 import net.noxinc.world.Map;
+import net.noxinc.world.cells.DebugCell;
 
 public class Main 
 {
-	Scanner myScanner = new Scanner(System.in);
 	public static void main(String[] args)
 	{
+		Scanner myScanner = new Scanner(System.in);
 		Map map  = new Map(10, 10);
-		Player player = new Player(map, 2, 3, 5);
+		Player player = new Player(map, 1, 1, 5);
 		
-		map.redraw();
-		player.getInventory().openInventory();
+		map.createCellAtPosistion(4, 4, new DebugCell());
 		
+		while(true)
+		{
+			map.redraw();
+			Dialogs.moveDialog();
+			player.move(myScanner.nextInt());
+		}
 	}
 
 }
