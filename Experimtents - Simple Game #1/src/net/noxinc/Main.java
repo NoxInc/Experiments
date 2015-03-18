a5972dbf465f9ef956a8990e54f947f71b46f9af
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-18 14:58:41 +0100

    Added basic functionality

--- Experimtents - Simple Game #1/src/net/noxinc/Main.java
+++ Experimtents - Simple Game #1/src/net/noxinc/Main.java
@@ -1,9 +1,19 @@
 package net.noxinc;
 
+import java.util.Scanner;
+
+import net.noxinc.units.Player;
+import net.noxinc.world.Map;
+
 public class Main 
 {
+	Scanner myScanner = new Scanner(System.in);
 	public static void main(String[] args)
 	{
+		Map map  = new Map(10, 10);
+		Player player = new Player(map, 2, 3);
+		
+		map.redraw();
 		
 	}
 
