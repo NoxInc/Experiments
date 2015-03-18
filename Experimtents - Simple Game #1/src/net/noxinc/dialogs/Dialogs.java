d4a9aab2213cf62ec1809d6f3515e9a5d1603831
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-18 14:48:26 +0100

    init static Dialogs (moving)

---
+++ Experimtents - Simple Game #1/src/net/noxinc/dialogs/Dialogs.java
@@ -0,0 +1,13 @@
+package net.noxinc.dialogs;
+
+public class Dialogs 
+{
+	public static void moveDialog()
+	{
+		System.out.println("1 up");
+		System.out.println("2 down");
+		System.out.println("3 right");
+		System.out.println("4 left");
+	}
+
+}
