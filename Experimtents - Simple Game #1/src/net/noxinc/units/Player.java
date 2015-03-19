ebf62960a1445259f5de60161581b2ce6a2f66dd
NoxInc <24u_49a9tbg8cu84@byom.de>
2015-03-19 03:14:49 +0100

    Improved movement and Cell collision detection

--- Experimtents - Simple Game #1/src/net/noxinc/units/Player.java
+++ Experimtents - Simple Game #1/src/net/noxinc/units/Player.java
@@ -47,78 +47,113 @@ public class Player extends Cell
 		switch(direction)
 		{
 		case 1:
-			if(y - 1 < 1)
-			{
-				break;
-			}else{
-				map.getBoard()[x][y] = tmpCell;
-				if(map.getCellAtPosition(x, y - 1).isCollectable())
-				{
-					playerInventory.addToInventory(map.getBoard()[x][y - 1]);
-					tmpCell = new EmptyCell();
-				}else{
-					tmpCell = map.getBoard()[x][y - 1];
-				}
-				map.getBoard()[x][y - 1] = tmpPlayer;
-				y--;
-			}
+			moveUp();
 			break;
 		case 2:
-			if(y + 1 > map.getYLength() - 2)
-			{
-				break;
-			}else{
-				map.getBoard()[x][y] = tmpCell;
-				if(map.getCellAtPosition(x, y + 1).isCollectable())
-				{
-					playerInventory.addToInventory(map.getBoard()[x][y + 1]);
-					tmpCell = new EmptyCell();
-				}else{
-					tmpCell = map.getBoard()[x][y + 1];
-				}
-				map.getBoard()[x][y + 1] = tmpPlayer;
-				y++;
-			}
+			moveDown();
 			break;
 		case 3:
-			if(x + 1 > map.getXLength() - 2)
-			{
-				break;
-			}else{
-				map.getBoard()[x][y] = tmpCell;
-				if(map.getCellAtPosition(x + 1, y).isCollectable())
-				{
-					playerInventory.addToInventory(map.getBoard()[x + 1][y]);
-					tmpCell = new EmptyCell();
-				}else{
-					tmpCell = map.getBoard()[x + 1][y];
-				}
-				map.getBoard()[x + 1][y] = tmpPlayer;
-				x++;
-			}
+			moveRight();
 			break;
 		case 4:
-			if(x - 1 < 1)
-			{
-				break;
-			}else{
-				map.getBoard()[x][y] = tmpCell;
-				if(map.getCellAtPosition(x - 1, y).isCollectable())
-				{
-					playerInventory.addToInventory(map.getBoard()[x - 1][y]);
-					tmpCell = new EmptyCell();
-				}else{
-					tmpCell = map.getBoard()[x - 1][y];
-				}
-				map.getBoard()[x - 1][y] = tmpPlayer;
-				x--;
-			}
+			moveLeft();
 			break;
 		case 5:
 			openInventory();
 			break;
 		default:
 			break;
+		}
+	}
+	private void moveUp()
+	{
+		tmpPlayer = map.getBoard()[x][y];
+		if(!(y - 1 < 1))
+		{
+			if(map.getCellAtPosition(x, y - 1).isCollectable())
+			{
+				map.getBoard()[x][y] = tmpCell;
+				playerInventory.addToInventory(map.getBoard()[x][y - 1]);
+				tmpCell = new EmptyCell();
+				map.getBoard()[x][y - 1] = tmpPlayer;
+				y--;
+			}else if((map.getCellAtPosition(x, y - 1).isSolid()))
+			{
+			}else{
+				map.getBoard()[x][y] = tmpCell;
+				tmpCell = map.getBoard()[x][y - 1];
+				map.getBoard()[x][y - 1] = tmpPlayer;
+				y--;
+			}
+		}
+	}
+	
+	private void moveDown()
+	{
+		tmpPlayer = map.getBoard()[x][y];
+		if(!(y + 1 > map.getYLength() - 2))
+		{
+			if(map.getCellAtPosition(x, y + 1).isCollectable())
+			{
+				map.getBoard()[x][y] = tmpCell;
+				playerInventory.addToInventory(map.getBoard()[x][y + 1]);
+				tmpCell = new EmptyCell();
+				map.getBoard()[x][y + 1] = tmpPlayer;
+				y++;
+			}else if((map.getCellAtPosition(x, y + 1).isSolid()))
+			{
+			}else{
+				map.getBoard()[x][y] = tmpCell;
+				tmpCell = map.getBoard()[x][y + 1];
+				map.getBoard()[x][y + 1] = tmpPlayer;
+				y++;
+			}
+		}
+	}
+	
+	private void moveRight()
+	{
+		tmpPlayer = map.getBoard()[x][y];
+		if(!(x + 1 > map.getXLength() - 2))
+		{
+			if(map.getCellAtPosition(x + 1, y).isCollectable())
+			{
+				map.getBoard()[x][y] = tmpCell;
+				playerInventory.addToInventory(map.getBoard()[x + 1][y]);
+				tmpCell = new EmptyCell();
+				map.getBoard()[x + 1][y] = tmpPlayer;
+				x++;
+			}else if((map.getCellAtPosition(x + 1, y).isSolid()))
+			{
+			}else{
+				map.getBoard()[x][y] = tmpCell;
+				tmpCell = map.getBoard()[x + 1][y];
+				map.getBoard()[x + 1][y] = tmpPlayer;
+				x++;
+			}
+		}
+	}
+	
+	private void moveLeft()
+	{
+		tmpPlayer = map.getBoard()[x][y];
+		if(!(y - 1 < 1))
+		{
+			if(map.getCellAtPosition(x - 1, y).isCollectable())
+			{
+				map.getBoard()[x][y] = tmpCell;
+				playerInventory.addToInventory(map.getBoard()[x - 1][y]);
+				tmpCell = new EmptyCell();
+				map.getBoard()[x - 1][y] = tmpPlayer;
+				x--;
+			}else if((map.getCellAtPosition(x - 1, y).isSolid()))
+			{
+			}else{
+				map.getBoard()[x][y] = tmpCell;
+				tmpCell = map.getBoard()[x - 1][y];
+				map.getBoard()[x - 1][y] = tmpPlayer;
+				x--;
+			}
 		}
 	}
 }
